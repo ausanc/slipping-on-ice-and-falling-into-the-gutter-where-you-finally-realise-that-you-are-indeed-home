dbcee00985ca3c66f6956f2529041ded29d42b92
Julia Meister <juliameister@users.noreply.github.com>
2019-02-02 17:23:42 +0000

    API endpoint completes a task.
    /api/complete/{userid}/{taskid} checks that the records exist and that the user and task houseids match.

--- web_server/routes/api.php
+++ web_server/routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/complete/{user_id}/{task_id}', 'TaskController@completeTask');
