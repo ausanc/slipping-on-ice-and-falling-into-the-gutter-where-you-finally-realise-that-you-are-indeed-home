576ca688a046de6fadff37f9b84e41f34733598d
Henry Gilbert <henrygilbert15@live.co.uk>
2019-02-03 04:18:46 +0000

    Marquees

--- web_server/resources/views/task_list.blade.php
+++ web_server/resources/views/task_list.blade.php
@@ -6,22 +6,23 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-6 offset-md-3">
+    <marquee behavior="alternate" direction=""><div class="col-md-6">
 
     @foreach ($tasks as $task)
         <div class="card border-primary mt-3">
-            <h5 class="card-header text-center">{{ $task->task_name }}</h5>
+            <marquee behavior="alternate" direction=""><h5 class="card-header text-center">{{ $task->task_name }}</h5></marquee>
             <div class="card-body p-0">
                 <table class="table table-hover m-0">
+                    <marquee behavior="alternate" direction="">{{ $task->task_description }}</marquee>
                     <tbody>
                         @foreach ($task->completed as $completed_task)
                             <tr>
-                                <th scope="row">{{ $completed_task->user->name }}</th>
+                                <th scope="row"><marquee behavior="alternate" direction=""><marquee behavior="" direction="up" height=20>{{ $completed_task->user->name }}</marquee></marquee></th>
                                 <td>
-                                    {{ $completed_task->created_at->diffforhumans() }}
+                                    <marquee behavior="alternate" direction=""><marquee behavior="" direction="down" height=20>{{ $completed_task->created_at->diffforhumans() }}</marquee></marquee>
                                 </td>
                                 <td>
-                                    {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',  $completed_task->created_at)->format('F j, Y') }} at {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',  $completed_task->created_at)->format('H:i') }}
+                                    <marquee behavior="alternate" direction=""><marquee behavior="alternate" direction="up" height=20>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',  $completed_task->created_at)->format('F j, Y') }} at {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',  $completed_task->created_at)->format('H:i') }}</marquee></marquee>
                                 </td>
                             </tr>
                         @endforeach
@@ -31,6 +32,6 @@
         </div>
     @endforeach
 
-    </div>
+    </div></marquee>
 </div>
 @endsection
